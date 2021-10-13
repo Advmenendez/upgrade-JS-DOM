c260f8ff5d50ca12bfee6ed76f0d685a76758c46
Your Name <advmenendez92@gmail.com>
2021-10-13 06:56:09 +0200

    final commit

--- Iteration2.js
+++ Iteration2.js
@@ -19,7 +19,11 @@ window.onload = function() {
 
 
 //2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
-
+const div2=document.createElement("div");
+for(let i=0;i<6;i++){
+    let p2=document.createElement("p");
+    div2.appendChild(p2);
+}
 
 
 
@@ -37,25 +41,27 @@ let newTexTwo = document.createTextNode("Wubba Lubba dub dub")
 myHTwo.appendChild(newTexTwo)
 
 /* 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
-const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter']; */
-
+ */
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const ul=document.createElement("ul");
+for(app of apps){
+    let li=document.createElement("li")
+    li.appendChild(document.createTextNode(app))
+    ul.appendChild(li)
+}
+document.body.appendChild(ul)
 //2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
-const remove = document.querySelectorAll('.fn-remove-me')
-let body = document.querySelector(".contenedor")
+let removes=document.querySelectorAll(".fn-remove-me").forEach(item => item.remove())
 
 
 /* 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
 	Recuerda que no solo puedes insertar elementos con .appendChild. */
 
-  let p = document.createElement("p")
-  let textP = document.createTextNode("Voy en medio")
-  let divs = document.querySelector(".fn-insert-heree")
-  p.appendChild(textP)
-  document.body.insertBefore(p, divs);
+  let div3= document.querySelectorAll("div")[1]
+let p4=document.createElement("p")
+p4.textContent="voy en medio!"
+document.body.insertBefore(p4,div3)
+console.log(div3)
 
 //2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
-  let pDentro = document.createElement("p")
-  let textDentro = document.createTextNode("Voy Dentro")
-  let allDivs = document.querySelectorAll(".fn-insert-here")
-  pDentro.appendChild(textDentro)
-  allDivs.appendChild(pDentro)
+document.querySelectorAll("div.fn-insert-here").forEach((div)=>div.innerHTML=`<p>Voy Dentro!</p>`);
